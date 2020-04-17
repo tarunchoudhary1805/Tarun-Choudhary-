2eb7614e47c14d9d09fbf0d7ca2954605a01024e
Tarun Choudhary <57765697+tarunchoudhary1805@users.noreply.github.com>
2020-04-17 16:13:12 +0530

    website

---
+++ src/Ruler.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Ruler() {
+    return (
+        <div id="ruler">
+        </div>
+    )
+}
+
+export default Ruler
---
+++ src/components/BioTarun.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function BioTarun() {
+    return (
+        <div>
+            {/* <DocumentTitle title="tarun choudhary"></DocumentTitle> */}
+            <h1 className="name">Hi, I'm TARUN</h1>
+            <p className="namep"> <i class="fa fa-code" aria-hidden="true"></i> FrontEnd Developer <i class="fa fa-code" aria-hidden="true"></i></p>
+        </div>
+    )
+}
+
+export default BioTarun
